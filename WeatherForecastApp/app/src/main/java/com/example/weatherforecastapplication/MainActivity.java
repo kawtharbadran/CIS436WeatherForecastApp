aabe8b20f184ad8f4c7127e0b64b9a3fe3515f4f
Kawthar Badran <37708354+kawtharbadran@users.noreply.github.com>
2020-04-11 15:45:02 -0400

    adding activity and XML

--- WeatherForecastApp/app/src/main/java/com/example/weatherforecastapplication/MainActivity.java
+++ WeatherForecastApp/app/src/main/java/com/example/weatherforecastapplication/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.weatherforecastapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the spinner by its ID and
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        Spinner spinner = findViewById(R.id.cities_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.cities_array, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears and
+        // apply the adapter to the spinner
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Something else
+            }
+        });
     }
 }
---
+++ WeatherForecastApp/app/src/main/java/com/example/weatherforecastapplication/weather_forecast.java
@@ -0,0 +1,14 @@
+package com.example.weatherforecastapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class weather_forecast extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_forecast);
+    }
+}
