4b8c655236595af4d0eaf1deac0ff17076f62298
asfiaa <38917576+asfiaa@users.noreply.github.com>
2020-04-11 19:28:23 -0400

    Selecting city leads to new activity with the city name

--- WeatherForecastApp/app/src/main/java/com/example/weatherforecastapplication/MainActivity.java
+++ WeatherForecastApp/app/src/main/java/com/example/weatherforecastapplication/MainActivity.java
@@ -8,17 +8,21 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.weatherforecastapplication.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Get the spinner by its ID and
         // Create an ArrayAdapter using the string array and a default spinner layout
-        Spinner spinner = findViewById(R.id.cities_spinner);
+        final Spinner spinner = findViewById(R.id.cities_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.cities_array, android.R.layout.simple_spinner_item);
 
@@ -32,6 +36,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+                if(position> 0 && position < 6 ) {
+                    Intent intent = new Intent(MainActivity.this, city_selected.class);
+                    TextView city = (TextView) findViewById(R.id.cityName);
+                    String message = spinner.getSelectedItem().toString();
+                    intent.putExtra(EXTRA_MESSAGE, message);
+                    startActivity(intent);
+                }
             }
 
             @Override
